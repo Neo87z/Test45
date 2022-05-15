4770cb6fa787027f0632b0989c62be5c21de3717
Neo87z <shehanhoradagoda12@gmail.com>
2022-05-15 00:06:48 -0230

    Ok e

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello SLIIT updated rom CICD!')
+  res.send('Hello SLIIT FK yea!')
 })
 
 app.listen(port, () => {
